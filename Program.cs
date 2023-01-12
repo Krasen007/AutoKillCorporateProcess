2420e3276ce18638c93e332542a1d5584847b5fc
Krasen <krasen007@gmail.com>
2023-01-12 19:33:15 +0200

    basic functionality

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Get the processes running on the local computer.
+        Process[] runningProcesses = Process.GetProcessesByName("notepad");
+
+        if (runningProcesses.Length == 0)
+        {
+            Console.WriteLine("No such process");
+        }
+        else
+        {
+            //DateTime.UtcNow
+            foreach (Process process in runningProcesses)
+            {
+                process.Kill();
+
+                Console.WriteLine(process.ProcessName + " " + process.Id + " killed at " + DateTime.UtcNow);
+                using (StreamWriter w = File.AppendText("log.txt"))
+                {                    
+                    w.WriteLine(process.ProcessName + " " + process.Id + " killed at " + DateTime.UtcNow);
+                }
+            }
+        }
+    }
+}
