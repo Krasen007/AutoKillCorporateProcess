4de9953063f695bb34448dfc8cec7339ee94cefe
Krasen <krasen007@gmail.com>
2023-01-12 23:08:20 +0200

    updated to check every 10 minutes

--- Program.cs
+++ Program.cs
@@ -4,26 +4,47 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Get the processes running on the local computer.
-        Process[] runningProcesses = Process.GetProcessesByName("notepad");
+        // Enter the name of the processes running on the local computer.
+        Start:
+        Console.WriteLine("Enter the name of the process: ");
+        string? annoyingProcess = Console.ReadLine();
+        if ((annoyingProcess == "") || (annoyingProcess == null))
+        {
+            Console.Clear();
+            goto Start;
+        }
+        bool noProc = false;
+        GetProcess:
+        Process[] runningProcesses = Process.GetProcessesByName(annoyingProcess);
 
         if (runningProcesses.Length == 0)
         {
-            Console.WriteLine("No such process");
+            if (noProc)
+            {
+                // Check every 10 minutes if the process is running.
+                Thread.Sleep(600000);
+                goto GetProcess;
+            }
+            else
+            {
+                Console.WriteLine("No such process is running, waiting...");
+                noProc = true;
+                Thread.Sleep(5000);
+                goto GetProcess;
+            }
         }
         else
         {
-            //DateTime.UtcNow
             foreach (Process process in runningProcesses)
             {
                 process.Kill();
+                Console.WriteLine("Success!");
+                noProc = false;
 
-                Console.WriteLine(process.ProcessName + " " + process.Id + " killed at " + DateTime.UtcNow);
-                using (StreamWriter w = File.AppendText("log.txt"))
-                {                    
-                    w.WriteLine(process.ProcessName + " " + process.Id + " killed at " + DateTime.UtcNow);
-                }
+                using StreamWriter logFile = File.AppendText("log.txt");
+                logFile.WriteLine(process.ProcessName + " " + process.Id + " killed at " + DateTime.UtcNow);
             }
+            goto GetProcess;
         }
     }
 }
